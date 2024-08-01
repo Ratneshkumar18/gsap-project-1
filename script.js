6f57673ddb86e2c35ea0cc50f9a05eac7aed449f
Ratneshkumar18 <143165126+Ratneshkumar18@users.noreply.github.com>
2024-08-01 20:15:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+gsap.to("#lft-1", {
+    x: 100,
+    duration:1
+})
+gsap.to("#lft-2", {
+    y: 50,
+    duration:1
+})
+gsap.to("#lft-3", {
+    y: -20,
+    duration:1
+})
+gsap.to("#lft-4", {
+    x: 30,
+    duration:1
+})
+
+gsap.to("#rght-1",{
+    y: 30,
+    duration:1
+})
+gsap.to("#rght-2",{
+    x: -30,
+    duration:1
+})
+gsap.to("#rght-3",{
+    y: -30,
+    duration:1
+})
+gsap.to("#rght-4",{
+    x: -30,
+    duration:1
+})
